dd3ec36b237afa18b777d4b61312643f84493162
Alex Valdez <alexvaldez31@tamu.edu>
2018-07-15 16:39:02 -0500

    From Section 3

--- base-react/src/App.js
+++ base-react/src/App.js
@@ -15,26 +15,51 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
 
       this.setState(
         {persons: [
-          { name: 'Daqwan', age: 21 },
+          { name: newName, age: 21 },
           { name: 'Jacob', age: 16 },
           { name: 'Kenzi', age: 18 },
         ]}
       )
   }
 
+  nameChangedHandler = (event) => {
+    this.setState(
+      {persons: [
+        { name: 'Alex', age: 21 },
+        { name: event.target.value, age: 16 },
+        { name: 'Kenzi', age: 20 },
+      ]}
+    )
+  }
+
+  
+
   
 
   render() {
+
+     
+      const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer',
+      }
+    
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button 
+        onClick={() => this.switchNameHandler('Hello')}
+        style={style}
+        >Switch Name</button>
         
         <Person 
           name={this.state.persons[0].name} 
@@ -43,6 +68,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this,'Max!')}
+          changed={this.nameChangedHandler}
           />
         <Person 
           name={this.state.persons[2].name} 
