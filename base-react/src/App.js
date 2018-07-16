bd7a2c84794a96fbe226916aa9d28ac5941e0f1f
Alex Valdez <alexvaldez31@tamu.edu>
2018-07-15 21:46:48 -0500

    Added some css styling

--- base-react/src/App.js
+++ base-react/src/App.js
@@ -100,11 +100,20 @@ class App extends Component {
 
         style.backgroundColor = 'red';
       }
+
+      // let classes = ['red' ,'bold'].join(' '); // for classes in App.css. You can see it in the <p></p>
+      let classes = [];
+      if (this.state.persons.length <= 2) {
+        classes.push('red');
+      }
+      if (this.state.persons.length <= 1) {
+        classes.push('bold');
+      }
     
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         
         <button 
         onClick={this.togglePersonsHandler}
